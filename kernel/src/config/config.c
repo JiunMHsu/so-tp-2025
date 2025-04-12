53007196694cebbe7936de4b10a283a166f62c4a
Jiun Ming Hsu <hsujm219@gmail.com>
2025-04-12 20:23:05 -0300

    declaracion func config, definicion de tipos auxiliares

---
+++ kernel/src/config/config.c
@@ -0,0 +1,19 @@
+#include "config.h"
+
+t_config *cpu_config;
+
+void iniciar_config(void) {}
+
+void destruir_config(void) {}
+
+char *get_puerto_escucha(puerto_escucha puerto) {}
+
+mem_address get_memoria_config(void) {}
+
+plani_corto_plazo get_alg_plani_corto_plazo(void) {}
+
+plani_largo_plazo get_alg_plani_largo_plazo(void) {}
+
+u_int32_t get_tiempo_suspension(void) {}
+
+t_log_level get_log_level(void) {}
---
+++ kernel/src/config/config.h
@@ -0,0 +1,84 @@
+#ifndef KERNEL_CONFIG_H
+#define KERNEL_CONFIG_H
+
+#include <stdlib.h>
+#include <string.h>
+#include <commons/config.h>
+#include <commons/log.h>
+#include <commons/collections/dictionary.h>
+
+#define CONFIG_FILE "kernel.config"
+
+typedef enum
+{
+    CPU_DISPATCH,
+    CPU_INTERRUPT,
+    IO
+} puerto_escucha;
+
+typedef struct
+{
+    char *ip;
+    char *puerto;
+} mem_address;
+
+typedef enum
+{
+    /**
+     * @brief (FIFO) First In First Out.
+     * No hay nada que explicar aca.
+     *
+     */
+    FIFO,
+
+    /**
+     * @brief (SJF) Shortest Job First.
+     * Evalúa (estima) el tiempo de ejecución de los procesos en READY
+     * y elige el de menor ráfaga.
+     *
+     */
+    SJF,
+
+    /**
+     * @brief (SRT) Shortest Remaining Time.
+     * Variante de SJF que agrega el desalojo de procesos.
+     *
+     * Replanifica cada vez que llega un proceso nuevo a READY,
+     * o cuando un proceso deja la CPU.
+     *
+     */
+    SRT
+} algoritmo_planificacion_corto_plazo;
+
+typedef enum
+{
+    /**
+     * @brief (FIFO) First In First Out.
+     * No hay nada que explicar aca.
+     *
+     */
+    FIFO,
+
+    /**
+     * @brief (SPF) Smallest Process First.
+     * Nombre inventado, en la consigna se enuncia como
+     * "Proceso más chico primero".
+     *
+     */
+    SPF
+} algoritmo_planificacion_largo_plazo;
+
+typedef algoritmo_planificacion_corto_plazo plani_corto_plazo;
+typedef algoritmo_planificacion_largo_plazo plani_largo_plazo;
+
+void iniciar_config(void);
+void destruir_config(void);
+
+char *get_puerto_escucha(puerto_escucha puerto);
+mem_address get_memoria_config(void);
+plani_corto_plazo get_alg_plani_corto_plazo(void);
+plani_largo_plazo get_alg_plani_largo_plazo(void);
+u_int32_t get_tiempo_suspension(void);
+t_log_level get_log_level(void);
+
+#endif // KERNEL_CONFIG_H
