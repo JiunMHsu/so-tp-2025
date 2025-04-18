79ce3d2d7e5a472e37ef960d4ae069d4c2b0e6d7
Jiun Ming Hsu <hsujm219@gmail.com>
2025-04-18 04:18:38 -0300

    cambios

--- kernel/src/recursos/cpu.c
+++ kernel/src/recursos/cpu.c
@@ -1,5 +1,7 @@
 #include "cpu.h"
 
+// colección global de CPUs
+
 void *manejar_conexion_dispatch(void *fd_cpu)
 {
     int32_t fd_dispatch = *((int32_t *)fd_cpu);
@@ -10,6 +12,8 @@ void *manejar_conexion_dispatch(void *fd_cpu)
         log_mensaje_error("Error cliente inválido");
         return NULL;
     }
+
+    // Escuchar identificador
 
     // TODO: Lógica de atención
     while (1)
@@ -31,6 +35,8 @@ void *manejar_conexion_interrupt(void *fd_cpu)
         log_mensaje_error("Error cliente inválido");
         return NULL;
     }
+
+    // Escuchar identificador
 
     // TODO: Lógica de atención
     while (1)
