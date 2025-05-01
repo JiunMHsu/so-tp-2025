ca13706889effa54f56d4975dee3a227d424f111
Jiun Ming Hsu <hsujm219@gmail.com>
2025-05-01 15:53:54 -0300

    ~

--- utils/src/utils/mqueue/mqueue.h
+++ utils/src/utils/mqueue/mqueue.h
@@ -12,9 +12,9 @@ typedef struct
 } t_mutex_queue;
 
 t_mutex_queue *crear_mutex_queue(void);
-void *pop_mutex_queue(t_mutex_queue *);
-void *peek_mutex_queue(t_mutex_queue *);
-void push_mutex_queue(t_mutex_queue *, void *);
-void destruir_mutex_queue(t_mutex_queue *);
+void *pop_mutex_queue(t_mutex_queue *mqueue);
+void *peek_mutex_queue(t_mutex_queue *mqueue);
+void push_mutex_queue(t_mutex_queue *mqueue, void *item);
+void destruir_mutex_queue(t_mutex_queue *mqueue);
 
 #endif // UTILS_MQUEUE_H
