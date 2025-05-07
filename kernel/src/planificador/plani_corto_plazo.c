55abe199fc22f1078b3e81b7b350741ea4880291
Jiun Ming Hsu <hsujm219@gmail.com>
2025-05-06 21:04:28 -0300

    cosas

--- kernel/src/planificador/plani_corto_plazo.c
+++ kernel/src/planificador/plani_corto_plazo.c
@@ -1,3 +1,9 @@
 #include "plani_corto_plazo.h"
 
 // TODO: implementar planificador de corto plazo
+
+q_estado *cola_ready;
+
+// 1. funcion inicializacion
+
+// funciones rutina
--- kernel/src/planificador/plani_largo_plazo.c
+++ kernel/src/planificador/plani_largo_plazo.c
@@ -1,3 +1,6 @@
 #include "plani_largo_plazo.h"
 
+q_estado *cola_new;
+q_estado *cola_ready;
+
 // TODO: implementar planificador de largo plazo
