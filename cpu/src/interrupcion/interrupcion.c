7b099284dacb0d05ad012e8cea2f208a4fd06266
Ramiro Vera <ravera@frba.utn.edu.ar>
2025-05-15 22:18:53 +0000

    gestion de interrupciones completa

--- cpu/src/interrupcion/interrupcion.c
+++ cpu/src/interrupcion/interrupcion.c
@@ -1,6 +1,7 @@
 #include "interrupcion.h"
 
 int32_t fd_interrupt;
+int8_t llego_interrupcion = 0;
 
 // otras globales de interrupcion
 
@@ -10,23 +11,50 @@ void inicializar_interrupcion(int32_t fd_interrupt)
 {
     pthread_t hilo_interrupt;
 
-    pthread_create(&hilo_interrupt, NULL, &atender_kernel_interrupt, NULL);
+    pthread_create(&hilo_interrupt, NULL, &atender_kernel_interrupt, fd_interrupt);
     pthread_detach(hilo_interrupt);
 }
 
-// TODO: Implementar la logica de interrupcion
-void *atender_kernel_interrupt(void *_)
+void *atender_kernel_interrupt(void *fd_ptr)
 {
     // escuchar de kernel
     // si le cae un int, setear el global
+
+    // TODO: el acceso a la variable global tiene que ser atomico, tendria que haber un mutex => cuando llamo a hay_interrupcion en otro hilo, podria estar modificando el valor aca
+    int32_t fd_interrupt = *((int32_t *)fd_ptr);
+    free(fd_ptr);
+
+    while (1)
+    {
+        int32_t senial_interrupcion = recibir_senial(fd_interrupt);
+
+        if (senial_interrupcion == -1)
+        {
+            log_mensaje_error("Error al recibir señal de interrupcion. Cerrando conexión con kernel...");
+            cerrar_conexion(fd_interrupt);
+            return NULL;
+        }
+
+        log_interrupcion_recibida();
+
+        // TODO: falta mutex
+        llego_interrupcion = 1;
+    }
+
     return NULL;
 }
 
-// TODO: Implementar la logica de interrupcion
 int8_t hay_interrupcion()
 {
-    return 0;
+    // TODO: falta mutex??
+    if (llego_interrupcion != 0)
+        return 1;
+
+    return -1;
 }
 
-// TODO: Implementar la logica de interrupcion
-void resetear_interrupcion() {}
+void resetear_interrupcion()
+{
+    // TODO: falta mutex
+    llego_interrupcion = 0;
+}
--- cpu/src/interrupcion/interrupcion.h
+++ cpu/src/interrupcion/interrupcion.h
@@ -5,6 +5,9 @@
 #include <pthread.h>
 #include <utils/protocol/protocol.h>
 
+#include "logger/logger.h"
+#include "conexion/kernel.h"
+
 void inicializar_interrupcion(int32_t fd_interrupt);
 int8_t hay_interrupcion(void);
 void resetear_interrupcion(void);
