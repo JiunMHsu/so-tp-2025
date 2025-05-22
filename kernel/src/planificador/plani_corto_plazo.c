9d8e149a3e3c50d888f0f7c7acd9c6fd00aa2a25
Francine Contini <francinecontini@gmail.com>
2025-05-22 03:08:18 +0000

    Planificador corto plazo con fifo

--- kernel/src/planificador/plani_corto_plazo.c
+++ kernel/src/planificador/plani_corto_plazo.c
@@ -1,3 +1,43 @@
 #include "plani_corto_plazo.h"
 
-// TODO: implementar planificador de corto plazo
+static algoritmo_planificacion algoritmo;
+static q_estado *ready;
+static double alpha;
+static double estimacion_inicial;
+
+
+void inicializar_planificador_corto_plazo(algoritmo_planificacion algoritmo_corto_plazo, q_estado *estado_ready)
+{
+    algoritmo = algoritmo_corto_plazo;
+    ready = estado_ready;
+    alpha = get_alfa_estimacion();
+    estimacion_inicial = //funcion del config que no encuentro
+
+}
+
+t_pcb *seleccionar_proximo_proceso() 
+{
+    pthread_mutex_lock(&mutex_ready);
+
+    t_pcb *proximo = NULL;
+
+    switch (algoritmo) {
+        case FIFO:
+            proximo = pop_proceso(ready);
+            break;
+        case SJF:
+            proximo = pop_sjf(ready); //TODO
+            break;
+        case SRT:
+            proximo = pop_srt(ready); //TODO
+            break;
+    }
+
+    pthread_mutex_unlock(&mutex_ready);
+    return proximo;
+}
+
+double estimar_rafaga(double anterior_estimado, double real_anterior) 
+{
+    return alpha * real_anterior + (1 - alpha) * anterior_estimado;
+}
