8897feb54b3e0e140d4caec721648866abe0f6be
Ramiro Vera <ravera@frba.utn.edu.ar>
2025-05-25 04:33:28 +0000

    funciones de desalojo, falta recibir desalojo

--- utils/src/utils/ejecucion/desalojo.c
+++ utils/src/utils/ejecucion/desalojo.c
@@ -2,20 +2,27 @@
 
 static t_packet *empaquetar_desalojo(t_desalojo *desalojo);
 
-// TODO: implementar crear_desalojo
 t_desalojo *crear_desalojo(u_int32_t pid, u_int32_t program_counter, motivo_desalojo motivo, char *syscall)
 {
-    return NULL;
+    t_desalojo *desalojo = malloc(sizeof(t_desalojo));
+    
+    desalojo->pid = pid;
+    desalojo->program_counter = program_counter;
+    desalojo->motivo = motivo;
+    desalojo->syscall = syscall; //TODO: revisar cuando se libera, creo que pasa en kernel
+
+    return desalojo;
 }
 
-// TODO: implementar destruir_desalojo
 void destruir_desalojo(t_desalojo *desalojo)
 {
+    free(desalojo); //TODO: no se si solo se libera la direccion de desalojo o si se debe liberar char *syscall tambien
 }
 
-// TODO: implementar enviar_desalojo
 void enviar_desalojo(int32_t fd_conexion, t_desalojo *desalojo)
 {
+    t_packet *paquete = empaquetar_desalojo(desalojo);
+    enviar_paquete(paquete, fd_conexion);
 }
 
 // TODO: implementar recibir_desalojo
@@ -24,8 +31,11 @@ t_desalojo *recibir_desalojo(int32_t fd_conexion)
     return NULL;
 }
 
-// TODO: implementar empaquetar_desalojo
 static t_packet *empaquetar_desalojo(t_desalojo *desalojo)
 {
-    return NULL;
+    t_packet *paquete = crear_paquete();
+    
+    agregar_a_paquete(paquete, desalojo, sizeof(t_desalojo));
+
+    return paquete;
 }
