8f8067e7420760dd13a8d1486066e3dc58efddd7
Jiun Ming Hsu <hsujm219@gmail.com>
2025-05-29 09:36:48 -0300

    completo destructor pcb

--- kernel/src/pcb/pcb.c
+++ kernel/src/pcb/pcb.c
@@ -3,6 +3,8 @@
 static void actualizar_metricas_estado(t_pcb *pcb);
 static void actualizar_metricas_tiempo(t_pcb *pcb);
 
+static void _destruir_lista_tiempos(void *elemento);
+
 char *get_estado_string(t_state estado)
 {
     switch (estado)
@@ -51,62 +53,75 @@ t_pcb *crear_pcb(u_int32_t pid, u_int32_t tamanio, char *ejecutable, u_int64_t e
     pcb->ultima_estimacion_rafaga = est_rafaga_inicial;
     pcb->estimacion_rafaga = -1;
 
-    pthread_mutex_init(&pcb->mutex, NULL);
+    pthread_mutex_init(&(pcb->mutex), NULL);
 
     return pcb;
 }
 
-// TODO: refactor, agregar destructores para los campos que lo requieran
 void destruir_pcb(t_pcb *pcb)
 {
-    free(pcb->ejecutable);
-    dictionary_destroy_and_destroy_elements(pcb->metricas_estado, free);
+    if (pcb == NULL)
+        return;
 
-    // si metricas de tiempo es una lista, recorrer y liberar
-    dictionary_destroy_and_destroy_elements(pcb->metricas_tiempo, free);
+    free(pcb->ejecutable);
+    dictionary_destroy_and_destroy_elements(pcb->metricas_estado, &free);
+    dictionary_destroy_and_destroy_elements(pcb->metricas_tiempo, &_destruir_lista_tiempos);
+
+    if (pcb->temporal != NULL)
+        temporal_destroy(pcb->temporal);
+
+    pthread_mutex_destroy(&(pcb->mutex));
     free(pcb);
+
+    pcb = NULL;
+}
+
+static void _destruir_lista_tiempos(void *elemento)
+{
+    t_list *lista_tiempos = (t_list *)elemento;
+    list_destroy_and_destroy_elements(lista_tiempos, &free);
 }
 
 t_state get_estado_pcb(t_pcb *pcb)
 {
-    pthread_mutex_lock(&pcb->mutex);
+    pthread_mutex_lock(&(pcb->mutex));
     t_state estado = pcb->estado;
-    pthread_mutex_unlock(&pcb->mutex);
+    pthread_mutex_unlock(&(pcb->mutex));
     return estado;
 }
 
 u_int64_t get_ultima_estimacion_rafaga_pcb(t_pcb *pcb)
 {
-    pthread_mutex_lock(&pcb->mutex);
+    pthread_mutex_lock(&(pcb->mutex));
     u_int64_t ultima_estimacion = pcb->ultima_estimacion_rafaga;
-    pthread_mutex_unlock(&pcb->mutex);
+    pthread_mutex_unlock(&(pcb->mutex));
     return ultima_estimacion;
 }
 
 u_int64_t get_estimacion_rafaga_pcb(t_pcb *pcb)
 {
-    pthread_mutex_lock(&pcb->mutex);
+    pthread_mutex_lock(&(pcb->mutex));
     u_int64_t estimacion = pcb->estimacion_rafaga;
-    pthread_mutex_unlock(&pcb->mutex);
+    pthread_mutex_unlock(&(pcb->mutex));
     return estimacion;
 }
 
 void set_program_counter_pcb(t_pcb *pcb, u_int32_t program_counter)
 {
-    pthread_mutex_lock(&pcb->mutex);
+    pthread_mutex_lock(&(pcb->mutex));
     pcb->program_counter = program_counter;
-    pthread_mutex_unlock(&pcb->mutex);
+    pthread_mutex_unlock(&(pcb->mutex));
 }
 
 void set_estado_pcb(t_pcb *pcb, t_state estado)
 {
-    pthread_mutex_lock(&pcb->mutex);
+    pthread_mutex_lock(&(pcb->mutex));
 
     pcb->estado = estado;
     actualizar_metricas_estado(pcb);
     actualizar_metricas_tiempo(pcb);
 
-    pthread_mutex_unlock(&pcb->mutex);
+    pthread_mutex_unlock(&(pcb->mutex));
 }
 
 static void actualizar_metricas_estado(t_pcb *pcb)
@@ -151,14 +166,14 @@ static void actualizar_metricas_tiempo(t_pcb *pcb)
 
 void set_ultima_estimacion_rafaga_pcb(t_pcb *pcb, u_int64_t ultima_estimacion)
 {
-    pthread_mutex_lock(&pcb->mutex);
+    pthread_mutex_lock(&(pcb->mutex));
     pcb->ultima_estimacion_rafaga = ultima_estimacion;
-    pthread_mutex_unlock(&pcb->mutex);
+    pthread_mutex_unlock(&(pcb->mutex));
 }
 
 void set_estimacion_rafaga_pcb(t_pcb *pcb, u_int64_t estimacion)
 {
-    pthread_mutex_lock(&pcb->mutex);
+    pthread_mutex_lock(&(pcb->mutex));
     pcb->estimacion_rafaga = estimacion;
-    pthread_mutex_unlock(&pcb->mutex);
+    pthread_mutex_unlock(&(pcb->mutex));
 }
