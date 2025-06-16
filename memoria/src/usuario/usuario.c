32dab05ea7681322ae10a792d7a4244e0852bb82
Alejo Menay <amenay@frba.utn.edu.ar>
2025-06-16 03:12:33 +0000

    Primera impletancion de memoria principal

--- memoria/src/usuario/usuario.c
+++ memoria/src/usuario/usuario.c
@@ -1,1 +1,43 @@
 #include "usuario.h"
+
+void inicializar_memoria_principal(t_memoria* memoria)
+{
+    int32_t tam_memoria = get_tam_memoria();
+    if(tam_memoria <= 0)
+    {
+
+    }
+
+    memoria->tam_pagina = get_tam_pagina();
+    if(memoria->tam_pagina <= 0)
+    {
+
+    }
+
+    memoria->cantidad_marcos = tam_memoria / memoria->tam_pagina;
+    memoria->memoria_principal = malloc(tam_memoria);
+    if(memoria->memoria_principal == NULL)
+    {
+
+    }
+
+    int32_t bytes_asignacion_marcos = ceil(memoria->cantidad_marcos / 8);
+    char * buffer = malloc (bytes_asignacion_marcos);
+    if(buffer == NULL)
+    {
+
+    }
+
+    memset(buffer, 0, bytes_asignacion_marcos);
+    memoria->asignacion_marcos = bitarray_create_with_mode(buffer, bytes_asignacion_marcos, LSB_FIRST);
+}
+
+void asignar_marco()
+{
+
+}
+
+void liberar_marco()
+{
+
+}
--- memoria/src/usuario/usuario.h
+++ memoria/src/usuario/usuario.h
@@ -3,5 +3,23 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <commons/collections/dictionary.h>
+#include <commons/collections/list.h>
+#include <commons/bitarray.h>
+
+#include "config/config.h"
+
+typedef struct 
+{
+    void *memoria_principal;
+    t_bitarray *asignacion_marcos;
+    int32_t cantidad_marcos;
+    int32_t tam_pagina;
+    FILE *archivo_swap;
+}t_memoria;
+
+void inicializar_memoria_principal(t_memoria *memoria);
+void asignar_marco();
+void liberar_marco();
 
 #endif // MEMORIA_USUARIO_H
