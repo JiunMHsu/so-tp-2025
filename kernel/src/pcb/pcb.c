691b67124605cedfa2a48690a8e7a8ec6b8b166e
Jiun Ming Hsu <hsujm219@gmail.com>
2025-06-16 20:17:18 -0300

    implemento sjf

--- kernel/src/pcb/pcb.c
+++ kernel/src/pcb/pcb.c
@@ -53,6 +53,7 @@ t_pcb *crear_pcb(u_int32_t pid, u_int32_t tamanio, char *ejecutable, u_int64_t e
 
     pcb->ultima_estimacion_rafaga = est_rafaga_inicial;
     pcb->estimacion_rafaga = -1;
+    pcb->ultima_rafaga = 0;
 
     pthread_mutex_init(&(pcb->mutex), NULL);
 
@@ -91,6 +92,24 @@ t_state get_estado_pcb(t_pcb *pcb)
     return estado;
 }
 
+u_int64_t get_tiempo_estado_actual_pcb(t_pcb *pcb)
+{
+    pthread_mutex_lock(&(pcb->mutex));
+
+    if (pcb->temporal == NULL)
+    {
+        pthread_mutex_unlock(&(pcb->mutex));
+        return 0;
+    }
+
+    temporal_stop(pcb->temporal);
+    u_int64_t transcurrido = temporal_gettime(pcb->temporal);
+    temporal_start(pcb->temporal);
+
+    pthread_mutex_unlock(&(pcb->mutex));
+    return transcurrido;
+}
+
 u_int64_t get_ultima_estimacion_rafaga_pcb(t_pcb *pcb)
 {
     pthread_mutex_lock(&(pcb->mutex));
@@ -105,6 +124,14 @@ u_int64_t get_estimacion_rafaga_pcb(t_pcb *pcb)
     u_int64_t estimacion = pcb->estimacion_rafaga;
     pthread_mutex_unlock(&(pcb->mutex));
     return estimacion;
+}
+
+u_int64_t get_ultima_rafaga_pcb(t_pcb *pcb)
+{
+    pthread_mutex_lock(&(pcb->mutex));
+    u_int64_t ultima_rafaga = pcb->ultima_rafaga;
+    pthread_mutex_unlock(&(pcb->mutex));
+    return ultima_rafaga;
 }
 
 void set_program_counter_pcb(t_pcb *pcb, u_int32_t program_counter)
--- kernel/src/planificador/plani_corto_plazo.c
+++ kernel/src/planificador/plani_corto_plazo.c
@@ -9,6 +9,8 @@ static sem_t *hay_proceso_ready;
 static double alpha;
 static double estimacion_inicial;
 
+static algoritmo_planificacion algoritmo_en_uso;
+
 static double estimar_rafaga(double anterior_estimado, double real_anterior);
 static t_pcb *_es_de_menor_rafaga(t_pcb *a, t_pcb *b);
 
@@ -38,12 +40,15 @@ void inicializar_planificador_corto_plazo(q_estado *q_ready, q_estado *q_executi
     {
     case FIFO:
         planificador_corto_plazo = &planificar_por_fifo;
+        algoritmo_en_uso = FIFO;
         break;
     case SJF:
         planificador_corto_plazo = &planificar_por_sjf;
+        algoritmo_en_uso = SJF;
         break;
     case SRT:
         planificador_corto_plazo = &planificar_por_srt;
+        algoritmo_en_uso = SRT;
         break;
     default:
         log_mensaje_error("Algoritmo de planificación de corto plazo no soportado.");
@@ -59,10 +64,13 @@ void inicializar_planificador_corto_plazo(q_estado *q_ready, q_estado *q_executi
     pthread_detach(rutinas[1]);
 }
 
-// TODO: Implementar inserción en READY
 void insertar_en_ready(t_pcb *proceso)
 {
-    // inserto a ready con los valores calculados
+    u_int64_t rafaga_estimacion = estimar_rafaga(proceso->ultima_estimacion_rafaga, proceso->ultima_rafaga);
+    set_estimacion_rafaga_pcb(proceso, rafaga_estimacion);
+
+    push_proceso(q_ready, proceso);
+    sem_post(hay_proceso_ready);
 }
 
 static double estimar_rafaga(double anterior_estimado, double real_anterior)
@@ -70,10 +78,12 @@ static double estimar_rafaga(double anterior_estimado, double real_anterior)
     return alpha * real_anterior + (1 - alpha) * anterior_estimado;
 }
 
-// TODO: Implementar criterio de menor ráfaga
-static t_pcb *_es_de_menor_rafaga(t_pcb *a, t_pcb *b)
+static t_pcb *_es_de_menor_rafaga(t_pcb *proceso_a, t_pcb *proceso_b)
 {
-    return NULL;
+    if (get_estimacion_rafaga_pcb(proceso_a) < get_estimacion_rafaga_pcb(proceso_b))
+        return proceso_a;
+
+    return proceso_b;
 }
 
 static void *planificar_por_fifo(void *_)
@@ -81,8 +91,10 @@ static void *planificar_por_fifo(void *_)
     while (1)
     {
         sem_wait(hay_cpu_libre);
+        sem_wait(hay_proceso_ready); // capaz es medio al pedo
         t_pcb *proceso = pop_proceso(q_ready);
-        ejecutar(proceso); // bloquante si no hay CPU libre
+
+        ejecutar(proceso);
         push_proceso(q_executing, proceso);
     }
 
@@ -94,7 +106,9 @@ static void *planificar_por_sjf(void *_)
     while (1)
     {
         sem_wait(hay_cpu_libre);
+        sem_wait(hay_proceso_ready); // capaz es medio al pedo
         t_pcb *proceso = pop_proceso_minimo(q_ready, &_es_de_menor_rafaga);
+
         ejecutar(proceso); // bloquante si no hay CPU libre
         push_proceso(q_executing, proceso);
     }
