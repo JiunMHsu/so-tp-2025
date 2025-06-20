4e8f8f84f8306f61baf0194d508f0a0a4cdc354e
Ramiro Vera <ravera@frba.utn.edu.ar>
2025-06-20 21:23:07 +0000

    cambio la logica de traduccion para que se envie un string con todas las entradas por nivel

--- cpu/src/mmu/mmu.c
+++ cpu/src/mmu/mmu.c
@@ -27,6 +27,7 @@ u_int32_t get_direccion_fisica(u_int32_t pid, u_int32_t direccion_logica)
 
     marco = cache_habilitada ? get_marco_cache(numero_pagina) : -1;
 
+    // TODO revisar si esta bien la logica de agregado a cache
     if (marco == -1)
     {
         log_cache_miss(pid, numero_pagina);
@@ -67,30 +68,35 @@ u_int32_t get_direccion_fisica(u_int32_t pid, u_int32_t direccion_logica)
 u_int32_t obtener_marco_de_memoria(u_int32_t pid, u_int32_t cantidad_niveles, u_int32_t numero_pagina, u_int32_t cantidad_entradas_tp)
 {
     u_int32_t entrada_nivel_x;
-    u_int32_t *entradas_por_nivel = malloc(cantidad_niveles * sizeof(u_int32_t));
+    char *entradas_por_nivel = string_new();
 
     for (u_int32_t nivel = 1; nivel <= cantidad_niveles; nivel++)
     {
         entrada_nivel_x = floor(numero_pagina / potencia(cantidad_entradas_tp, (cantidad_niveles - nivel))) % cantidad_entradas_tp;
 
-        entradas_por_nivel[nivel - 1] = entrada_nivel_x; // Al nivel 1 le corresponde el indice 0 y así
+        // si no queda un espacion al final => al hacer split queda un elemento "" al final
+        if (nivel < cantidad_niveles)
+        {
+            string_append_with_format(&entradas_por_nivel, "%d ", entrada_nivel_x);
+        }
+        else
+        {
+            string_append_with_format(&entradas_por_nivel, "%d", entrada_nivel_x);
+        }
     }
 
-    // enviar mem request y recibir marco
-    // En vez de mandar una pagina se manda un array con las entradas por nivel que debe ir accediendo la memoria para llegar al marco
-    // TODO revisar implementacion en mem_request
-    enviar_peticion_marco(pid, cantidad_niveles, entradas_por_nivel);
+    enviar_peticion_marco(pid, entradas_por_nivel);
 
     u_int32_t marco = recibir_marco();
 
-    // TODO no se si va aca, estaria liberando despues de recibir respuesta por ende memoria no deberia resultar perjudicada
-    // CUANDO SE DEBERIA LIBERAR ESTE ARRAY??
+    // TODO revisar si esta bien esta liberacion => entiendo que si
+    // porque en el envio se hace una copia del string entonces el original (este) no se libera con la destruccion del pedido
     free(entradas_por_nivel);
 
     return marco;
 }
 
-// No tengo ganas de romperme la cabeza con problemas de tipo => pow recibe doubles y devuelve doubles, se vuelve medio pesado solucionar
+// No tengo ganas de romperme la cabeza con problemas de tipo => pow recibe doubles y devuelve doubles
 u_int32_t potencia(u_int32_t base, u_int32_t exponente)
 {
     u_int32_t resultado = 1;
