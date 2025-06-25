95877b10c3ec66ddcbc84bf084a0475f357ea23c
Ramiro Vera <ravera@frba.utn.edu.ar>
2025-06-25 02:41:23 +0000

    agrego logica de accion con cache y memoria

--- cpu/src/instrucciones/instrucciones.c
+++ cpu/src/instrucciones/instrucciones.c
@@ -41,29 +41,53 @@ static void _write(char **parametros)
     // 1. direccion logica
     // 2. dato a escribir en memoria principal (o memoria cache si es que la pagina se encuentra en ella)
 
-    //fijarse si hay cache habilitada
-    //si esta habilitada => operar en cache
-    //si NO esta habilitada => operar en memoria => traduccion de direccion logica a fisica
+    // fijarse si hay cache habilitada
+    // si esta habilitada => operar en cache
+    // si NO esta habilitada => operar en memoria => traduccion de direccion logica a fisica
 
     // operacion en cache_
     // pagina en cache => operar directamente
     // pagina NO esta en cache:
     // CASO 1: tlb habilitada => buscar marco en tlb => pedir a memoria la pagina de ese marco => cargarla en cache => operar en cache
     // CASO 2: tlb NO habilitada => buscar pagina directamente en memoria y cargarla => operar en cache
+    u_int32_t direccion_logica = atoi(parametros[0]);
+    void *datos = (void *)parametros[1];
+
+    if (cache_habilitada())
+    {
+        u_int32_t nro_pagina = get_nro_pagina(direccion_logica);
+        u_int32_t offset = get_offset(direccion_logica);
+
+        if (existe_pagina_cache(nro_pagina))
+        {
+            // pedir frame a MMU
+            // pedir pagina en ese frame a memoria => enviar_peticion_pagina()
+            u_int32_t marco_pagina = get_marco(direccion_logica);
+            void *contenido_pagina = enviar_peticion_pagina(get_pid(), marco);
+            cachear_pagina(nro_pagina, contenido_pagina);
+        }
+
+        escribir_cache(nro_pagina, offset, datos)
+    }
+    else
+    {
+        u_int32_t direccion_fisica = get_direccion_fisica(get_pid(), direccion_logica);
+        enviar_peticion_escritura(get_pid(), direccion_fisica, datos);
+    }
 }
 
 static void _read(char **parametros)
 {
-    //descomponer parametros
-    //1. direccion logica
-    //2. tamaño de dato a leer
+    // descomponer parametros
+    // 1. direccion logica
+    // 2. tamaño de dato a leer
 
     // fijarse si hay cache habilitada
     // si esta habilitada => operar en cache
     // si NO esta habilitada => operar en memoria => traduccion de direccion logica a fisica
 
-    //printear el dato leido
-    //loggearlo
+    // printear el dato leido
+    // loggearlo
 }
 
 static void go_to(char **parametros)
