19eb93bba60797527da8acddb3fcef5df98d04b0
Ramiro Vera <ravera@frba.utn.edu.ar>
2025-07-08 02:27:13 +0000

    cambio funcion de inicializacion para tener un flag interno, deberia llamarse en el main de cpu

--- cpu/src/cache/cache.c
+++ cpu/src/cache/cache.c
@@ -2,19 +2,25 @@
 
 static entrada_cache *memoria_cache;
 static algoritmo_sustitucion algoritmo;
+static u_int32_t cantidad_entradas;
+static u_int32_t habilitada;
 
-u_int32_t inicializar_cache()
+void inicializar_cache()
 {
-    u_int32_t cantidad_entradas = get_entradas_cache();
+    cantidad_entradas = get_entradas_cache();
 
-    if (cantidad_entradas == 0)
-        return 0;
+    if (cantidad_entradas != 0)
+    {
+        habilitada = 0;
+    }
+    else
+    {
+        habilitada = 1;
 
-    memoria_cache = malloc(cantidad_entradas * sizeof(entrada_cache));
+        memoria_cache = malloc(cantidad_entradas * sizeof(entrada_cache));
 
-    algoritmo = get_algoritmo_sustitucion_cache();
-
-    return 1;
+        algoritmo = get_algoritmo_sustitucion_cache();
+    }
 }
 
 // TODO: funciones de cache
@@ -23,22 +29,22 @@ void agregar_entrada_cache(u_int32_t numero_pagina, int32_t marco)
 {
 }
 
-u_int32_t pagina_en_cache(u_int32_t pagina)
+void escribir_cache(u_int32_t nro_pagina, u_int32_t offset, void *datos, u_int32_t buffer_size)
 {
-    return 0;
+    entrada_cache *entrada = get_entrada(nro_pagina);
+    entrada->bit_modificado = 1;
+
+    memcpy(entrada->contenido + offset, datos, buffer_size);
 }
 
-u_int32_t cache_habilitada()
+void *leer_cache(u_int32_t nro_pagina, u_int32_t bytes_tamanio)
 {
-    return 0;
-}
+    entrada_cache *entrada = get_entrada(nro_pagina);
+    void *datos_leidos = malloc(bytes_tamanio); // TODO LIBERAR
 
-void escribir_cache(u_int32_t numero_pagina, u_int32_t offset, void *datos)
-{
-}
+    memcpy(datos_leidos, entrada->contenido, bytes_tamanio);
 
-void *leer_cache(u_int32_t numero_pagina, u_int32_t bytes_tamanio)
-{
+    return datos_leidos;
 }
 
 entrada_cache crear_entrada(u_int32_t pagina, void *contenido, u_int32_t bit_uso, u_int32_t bit_modificado)
@@ -52,6 +58,36 @@ entrada_cache crear_entrada(u_int32_t pagina, void *contenido, u_int32_t bit_uso
     nueva_entrada.bit_modificado = 0;
 
     return nueva_entrada;
+}
+
+entrada_cache *get_entrada(u_int32_t nro_pagina)
+{
+    for (u_int32_t i = 0; i < cantidad_entradas; i++)
+    {
+        if (memoria_cache[i].pagina == nro_pagina)
+        {
+            memoria_cache[i].bit_uso = 1;
+            return &memoria_cache[i];
+        }
+    }
+}
+
+u_int32_t existe_pagina_cache(u_int32_t nro_pagina)
+{
+    for (u_int32_t i = 0; i < cantidad_entradas; i++)
+    {
+        entrada_cache entrada = memoria_cache[i];
+
+        if (entrada.pagina == nro_pagina)
+            return 1;
+    }
+
+    return 0;
+}
+
+u_int32_t cache_habilitada()
+{
+    return habilitada;
 }
 
 void destruir_cache() {}
