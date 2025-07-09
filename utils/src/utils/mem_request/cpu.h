a564a10e7b6d88c476be6a833aff18b8f31d4182
Jiun Ming Hsu <hsujm219@gmail.com>
2025-07-09 19:04:32 -0300

    refactor firma cpu mem request

--- utils/src/utils/mem_request/cpu.h
+++ utils/src/utils/mem_request/cpu.h
@@ -20,7 +20,7 @@ typedef struct
     operacion_cpu_memoria operacion;
     u_int32_t pid;
     u_int32_t program_counter;  // Presente solo para FETCH_INSTRUCCION
-    char *entradas_por_nivel;   // Presente solo para OBTENER_MARCO // TODO: Revisar
+    char *entradas_por_nivel;   // Presente solo para OBTENER_MARCO
     u_int32_t direccion_fisica; // Presente para LEER y ESCRIBIR
     u_int32_t frame;            // Presente para LEER_PAG y ESCRIBIR_PAG
     u_int32_t tamanio_buffer;   // Presente para LEER y ESCRIBIR
@@ -31,6 +31,8 @@ t_peticion_cpu *crear_peticion_instruccion(u_int32_t pid, u_int32_t program_coun
 t_peticion_cpu *crear_peticion_nro_marco(u_int32_t pid, char *entradas_por_nivel);
 t_peticion_cpu *crear_peticion_lectura(u_int32_t pid, u_int32_t direccion_fisica, u_int32_t tamanio_buffer);
 t_peticion_cpu *crear_peticion_escritura(u_int32_t pid, u_int32_t direccion_fisica, u_int32_t tamanio_buffer, void *buffer);
+t_peticion_cpu *crear_peticion_lectura_pagina(u_int32_t pid, u_int32_t frame, u_int32_t tamanio_buffer);
+t_peticion_cpu *crear_peticion_escritura_pagina(u_int32_t pid, u_int32_t frame, void *buffer);
 
 void enviar_peticion_cpu(int32_t fd_memoria, t_peticion_cpu *peticion);
 t_peticion_cpu *recibir_peticion_cpu(int32_t fd_conexion);
