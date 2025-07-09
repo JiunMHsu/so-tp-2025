2bdc5902709f9f64e3514b9364092f70e9b70610
Jiun Ming Hsu <hsujm219@gmail.com>
2025-07-09 06:31:41 -0300

    estructura archivo memory dump

---
+++ memoria/src/dump/dump.c
@@ -0,0 +1,14 @@
+#include "dump.h"
+
+// TODO: Implementar la función generar_dump
+void generar_dump(u_int32_t pid, t_list *paginas)
+{
+    // generar el nombre del archivo de dump
+    // crear el archivo de dump
+
+    // iterar sobre la lista de páginas
+    // ir escribiendo las páginas en el archivo de dump
+
+    // cerrar el archivo de dump
+    // logear que se generó el dump exitosamente
+}
