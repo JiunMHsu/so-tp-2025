33390a11c1b0bc53a1105b29de778a9ad86f012c
Ramiro Vera <ravera@frba.utn.edu.ar>
2025-07-10 01:14:35 +0000

    obtener direccion por marco, borro comentario, cambio log mal ubicado

--- cpu/src/mmu/mmu.c
+++ cpu/src/mmu/mmu.c
@@ -29,19 +29,15 @@ u_int32_t get_marco(u_int32_t direccion_logica)
     u_int32_t pid = get_pid();
     int32_t marco;
 
-    // revisar cache => si hay cache miss loggear => si hay cache hit escribir/leer ahi mismo (dejar como modificado si esta escrito)
-    // hubo cache miss => revisar tlb => si hay tlb miss loggear => si hay tlb hit agregar pagina a cache (si esta habilitada) => pedir pagina a memoria => agregar => si no, pedir marco a memoria calcular direccion => operacion
-    // hubo tlb miss => pedir a memoria => agregar pagina a cache (si esta habilitada) => escribir ahi => si no, obtener marco => direccion => operacion
-
     marco = tlb_habilitada ? get_marco_tlb(numero_pagina) : -1;
 
     if (marco == -1)
     {
-        log_tlb_miss(pid, numero_pagina);
         marco = obtener_marco_de_memoria(pid, cantidad_niveles, numero_pagina, cantidad_entradas_tp);
 
         if (tlb_habilitada)
         {
+            log_tlb_miss(pid, numero_pagina);
             agregar_entrada_tlb(numero_pagina, marco);
             log_pagina_ingresada_tlb(numero_pagina, marco);
         }
@@ -49,12 +45,6 @@ u_int32_t get_marco(u_int32_t direccion_logica)
     else
     {
         log_tlb_hit(pid, numero_pagina);
-
-        if (cache_habilitada())
-        {
-            agregar_entrada_cache(numero_pagina, marco);
-            log_pagina_ingresada_cache(pid, numero_pagina);
-        }
     }
 
     log_obtener_marco(pid, numero_pagina, marco);
@@ -64,6 +54,11 @@ u_int32_t get_marco(u_int32_t direccion_logica)
 u_int32_t get_direccion_fisica(u_int32_t direccion_logica)
 {
     return get_marco(direccion_logica) * tamanio_pagina + get_offset(direccion_logica);
+}
+
+u_int32_t get_direccion_fisica_por_marco(u_int32_t marco)
+{
+    return marco * tamanio_pagina;
 }
 
 static u_int32_t obtener_marco_de_memoria(u_int32_t pid, u_int32_t cantidad_niveles, u_int32_t numero_pagina, u_int32_t cantidad_entradas_tp)
