f2f406449ebb758bef8538fd693e42279df57d83
Ramiro Vera <ravera@frba.utn.edu.ar>
2025-07-10 00:43:28 +0000

    cambio dependencia

--- cpu/src/conexion/memoria.h
+++ cpu/src/conexion/memoria.h
@@ -6,7 +6,7 @@
 #include <utils/sockets/sockets.h>
 #include <utils/protocol/protocol.h>
 #include <utils/mem_request/cpu.h>
-#include <utils/mem_request/mem_response.h>
+#include <utils/mem_response/mem_response.h>
 
 #include "config/config.h"
 #include "logger/logger.h"
